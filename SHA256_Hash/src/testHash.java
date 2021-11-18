b4047e25c947d437bf51a843f743cd17eb2de05d
jeremy-mac <yang1556@purdue.edu>
2021-11-18 14:46:30 -0500

    Created test files to test SHA-256 hashing

--- SHA256_Hash/src/testHash.java
+++ SHA256_Hash/src/testHash.java
@@ -1,39 +1,43 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.io.File;
 
 public class testHash {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        Path path = Paths.get("/Users/jeremy/Desktop/CryptoFileSystemAuthentication/KingJamesBible.txt");
-        BufferedReader reader = Files.newBufferedReader(path);
-        String line = Files.readAllLines(path).get(0);
-        String m1 = line;
-        String m2 = line;
-        System.out.println(m1);
-        byte[] c1 = digest.digest(
-                m1.getBytes(StandardCharsets.UTF_8));
-        byte[] c2 = digest.digest(
-                m2.getBytes(StandardCharsets.UTF_8));
-        System.out.println(bytesToHex(c1));
-        System.out.println(bytesToHex(c2));
-    }
-    private static String bytesToHex(byte[] hash) {
-        StringBuilder hexString = new StringBuilder(2 * hash.length);
-        for (int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString(0xff & hash[i]);
-            if(hex.length() == 1) {
-                hexString.append('0');
+        File folder = new File("./");
+        File[] fileList = folder.listFiles();
+        MessageDigest md_Alice;
+        String hex_Alice;
+        assert fileList != null;
+        for (File file : fileList) {
+            if (file.isFile() && file.getName().endsWith(".txt")) {
+                md_Alice = MessageDigest.getInstance("SHA-256");
+                System.out.println(file.toString());
+                hex_Alice = checksum(file.toString(), md_Alice);
+                System.out.println(hex_Alice);
             }
-            hexString.append(hex);
         }
-        return hexString.toString();
+    }
+
+    private static String checksum(String filepath, MessageDigest md) throws IOException {
+
+        // file hashing with DigestInputStream
+        try (DigestInputStream dis = new DigestInputStream(new FileInputStream(filepath), md)) {
+            while (dis.read() != -1) ; //empty loop to clear the data
+            md = dis.getMessageDigest();
+        }
+
+        // bytes to hex
+        StringBuilder result = new StringBuilder();
+        for (byte b : md.digest()) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+
     }
 }
 
