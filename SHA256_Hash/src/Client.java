3b06206272d6886d5a4566ab835f0ef429bda3b1
Connor Cai <caicon18@gmail.com>
2021-11-18 23:50:54 -0500

    Added socket communication

---
+++ SHA256_Hash/src/Client.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+
+class Client {
+    public static void main(String args[]) throws Exception {
+        Socket s = new Socket("localhost", 3915);
+
+        /* start sending hashes */
+        SendHash sendHash = new SendHash(s);
+        Thread sender = new Thread(sendHash);
+        sender.start();
+
+        /* start receiving and validating hashes */
+        VerifyHash verifyHash = new VerifyHash(s);
+        Thread receiver = new Thread(verifyHash);
+        receiver.start();
+
+        /* wait for threads to finish */
+        sender.join();
+        receiver.join();
+
+        /* close socket */
+        s.close();
+
+    }
+}
