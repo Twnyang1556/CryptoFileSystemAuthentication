56e511e8c550fae939aab2a3c25ce6b0f66123cb
Connor Cai <36058668+caicon18@users.noreply.github.com>
2021-11-22 17:56:55 -0500

    Revert "OKAY THIS WILL WORK"

--- SHA256_Hash/src/Server.java
+++ SHA256_Hash/src/Server.java
@@ -10,15 +10,16 @@ class Server {
         ServerSocket ss = new ServerSocket(3915);
         Socket s = ss.accept();
 
+
+        /* start sending hashes */
+        SendHash sendHash = new SendHash(s);
+        Thread sender = new Thread(sendHash);
+        sender.start();
+
         /* start receiving and validating hashes */
         VerifyHash verifyHash = new VerifyHash(s);
         Thread receiver = new Thread(verifyHash);
         receiver.start();
-
-        /* start sending hashes */
-        SendHash sendHash = new SendHash(s, verifyHash);
-        Thread sender = new Thread(sendHash);
-        sender.start();
 
         /* wait for threads to finish */
         sender.join();
