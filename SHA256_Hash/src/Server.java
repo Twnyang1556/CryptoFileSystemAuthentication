cd4f0a609f290e052c98486b02f14de8908573dd
jeremy-mac <yang1556@purdue.edu>
2021-11-30 14:20:49 -0500

    Allow users to input file to verify.

--- SHA256_Hash/src/Server.java
+++ SHA256_Hash/src/Server.java
@@ -1,6 +1,6 @@
 import java.net.*;
 import java.io.*;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 class Server {
 
@@ -16,14 +16,17 @@ class Server {
         }
         Socket s = ss.accept();
 
-        String fileName = "./E_coli.txt";
+        Scanner myobj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter file name: ");
+
+        String fileName = myobj.nextLine();  // Read user input
         IO io = new IO(s);
 
         /* Hash server file folder */
         String hashSHA1 = HashGen.generateHashOfFile(fileName, "SHA-1");
         String hashSHA256 = HashGen.generateHashOfFile(fileName, "SHA-256");
-        System.out.println(hashSHA256);
-        System.out.println(hashSHA1);
+//        System.out.println(hashSHA256);
+//        System.out.println(hashSHA1);
         /* send hash to client*/
         io.write(hashSHA1);
 
